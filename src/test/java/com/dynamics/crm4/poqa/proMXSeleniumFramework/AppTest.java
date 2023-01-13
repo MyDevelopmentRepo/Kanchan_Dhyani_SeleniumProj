bf838fd106b8692d6d4e71407e88ee32ee127931
kanchan <kanchan,dhyani@gmail.com>
2023-01-13 12:51:52 +0100

    initial commit

---
+++ src/test/java/com/dynamics/crm4/poqa/proMXSeleniumFramework/AppTest.java
@@ -0,0 +1,20 @@
+package com.dynamics.crm4.poqa.proMXSeleniumFramework;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
